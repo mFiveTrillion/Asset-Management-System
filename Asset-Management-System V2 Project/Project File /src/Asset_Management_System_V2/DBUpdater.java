23652503e91096d37d611ffc78c75c9099d6bbe6
mFiveTrillion <hayden.richardmarsters@gmail.com>
2023-06-07 18:07:11 +1200

    create DBUpdater.java

---
+++ Asset-Management-System V2 Project/Project File /src/Asset_Management_System_V2/DBUpdater.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Asset_Management_System_V2;
+
+/**
+ *
+ * @author hayden
+ */
+public class DBUpdater {
+    
+}
